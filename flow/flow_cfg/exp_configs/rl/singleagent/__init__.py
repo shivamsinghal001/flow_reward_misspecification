2e2fe54f99b0c2398164fb84c18126bee179e6f1
Shivam Singhal <shivamsinghal@perceptron.bair.berkeley.edu>
2023-04-06 01:06:55 -0700

    updating init for baseline envs

--- flow/flow_cfg/exp_configs/rl/singleagent/__init__.py
+++ flow/flow_cfg/exp_configs/rl/singleagent/__init__.py
@@ -1,1 +1,1 @@
-from . import singleagent_traffic_light_grid, singleagent_ring, singleagent_merge_bus, singleagent_merge_accel, singleagent_merge, singleagent_figure_eight, singleagent_bottleneck
+from . import singleagent_traffic_light_grid, singleagent_ring, singleagent_merge_bus, singleagent_merge_accel, singleagent_merge, singleagent_figure_eight, singleagent_bottleneck, singleagent_bottleneck_baseline, singleagent_merge_baseline, singleagent_merge_bus_baseline
