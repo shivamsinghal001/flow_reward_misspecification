517c61585d0c20f3d8472dcc8f747a494a8a3846
Cassidy Laidlaw <cassidy256@gmail.com>
2023-04-18 09:57:55 -0700

    Fix NoneType error

--- flow/envs/base.py
+++ flow/envs/base.py
@@ -378,12 +378,13 @@ class Env(gym.Env, metaclass=ABCMeta):
             if self.get_additional_rl_control_info() is not None:
                 acc_controller_actions = self.get_additional_rl_control_info()
 
-            assert isinstance(self.action_space, Box)
-            acc_controller_actions = np.clip(
-                acc_controller_actions,
-                self.action_space.low,
-                self.action_space.high,
-            )
+            if acc_controller_actions:
+                assert isinstance(self.action_space, Box)
+                acc_controller_actions = np.clip(
+                    acc_controller_actions,
+                    self.action_space.low,
+                    self.action_space.high,
+                )
             infos["acc_controller_actions"] = acc_controller_actions
 
             if self.is_baseline:
